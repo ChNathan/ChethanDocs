c2ecd61f612a078e30f28420704dea4380a5739c
ChNathan <66319564+ChNathan@users.noreply.github.com>
2025-07-31 21:03:42 -0500

    Update index.tsx

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Docusaurus Tutorial - :inf:min ⏱️
           </Link>
         </div>
       </div>
